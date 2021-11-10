3f148434be4eaedab53c3cab6bb68ff4dce77a6a
unknown <jagg499@gmail.com>
2021-11-10 12:57:52 -0500

    PPG2-14 ui correspondiente al inicio de sesion y login de usuario

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,99 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+  <nav>
+
+    <img v-bind:src="require('./assets/we logo.png')" alt="" id="brand"/>
+    <div>
+      <form action="" id="form-login">
+        <div class="form-group">
+          <label id="name-label" for="name">e-mail</label>
+          <input type="text" name="name" placeholder="Ingresa tu e-mail" required/>
+       </div>
+        <div class="form-group">
+          <label id="name-label" for="name">password</label>
+          <input type="password"  placeholder="Ingresa tu contraseña" required/>
+       </div>
+       <button type="submit">LogIn</button>
+      </form>
+    </div>
+  </nav>
+
+
+  <div class="main-component">
+    <router-view
+      v-on:completedLogIn="completedLogIn"
+      v-on:completedSignUp="completedSignUp"
+      v-on:logOut="logOut"
+      v-on:completedTransaction="completedTransaction"
+    >
+    </router-view>
   </div>
-  <router-view/>
+
 </template>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
 
-#nav {
-  padding: 30px;
-}
+  body {
+    font-family: Arial, Helvetica, sans-serif;
+    margin: 0;
+    padding: 0;
+  }
 
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
+  #brand {
+    width: 100px;
+    height: 100px;
+    margin: 10px 26px;
+  }
 
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
+  nav {
+    display: flex;
+    position: fixed;
+    top: 0;
+    width: 100%;
+    right: 0;
+    justify-content: space-between;
+    background-color: #f1f1f1;
+  }
+
+  #form-login{
+    display: flex;
+  }
+
+  #form-login .form-group{
+    display: flex;
+    padding: 21px;
+    flex-direction: column;
+  }
+
+  #form-login input {
+    margin: 5px 0px;
+    padding: 12px;
+    border-radius: 18px;
+    border: none;
+  }
+
+  #form-login button{
+    width: 109px;
+    height: 38px;
+    margin-top: 54px;
+    margin-right: 36px;
+    border-radius: 15px;
+    border: none;
+    background-color: #a8baf0;
+    color: white;
+    font-size: 18px;
+  }
+
+  #form-login label{
+    margin-left: 12px;
+    font-size: 18px;
+    font-weight: 100;
+    margin-top: 9px;
+    color: #6a6a6a;  
+  }
+  .main-component{
+    width: 100%;
+    margin: 0;
+    padding: 0;
+  }
+
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,30 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="home-component">
+      <div>
+        <img v-bind:src="require('../assets/illustration.jpg')" alt="" id="illustration"/>
+      </div>
+      <div class="signUpform">
+        <h1>¿No tienes cuenta?</h1>
+        <form action="">
+          <div class="form-group">
+            <label for="">Nombre</label>
+            <input type="text" placeholder="Ingresa tu nombre"/>
+          </div>
+          <div class="form-group">
+            <label for="">Correo</label>
+            <input type="text" placeholder="example@example.com"/>
+          </div>
+          <div class="form-group">
+            <label for="">Username</label>
+            <input type="text" placeholder="Ingresa tu username"/>
+          </div>
+          <div class="form-group">
+            <label for="" placeholder="Ingresa tu password">Password</label>
+            <input type="text"/>
+          </div>
+          <button type="submit">SignUp</button>
+        </form>
+      </div>
   </div>
 </template>
 
@@ -16,3 +39,63 @@ export default {
   }
 }
 </script>
+<style>
+
+  .home-component{
+    background: white;
+    height: 657px;
+    width: 100%;
+    display: flex;
+  }
+
+  #illustration{
+    width: 781px;
+    margin-top: 99px;
+  }
+
+  .test {
+    background: #d9ebfbbd;
+    height: 657px;
+    width: 100%;
+  }
+  .signUpform{
+    margin-top: 150px;
+    margin-left: 100px;
+  }
+  .signUpform h1{
+    font-weight: 100;
+    color: #424242;
+    margin-bottom: 10px;
+  }
+
+  .signUpform input{
+    margin: 5px 0px;
+    padding: 12px;
+    width: 129%;
+    border-radius: 18px;
+    border: 1px solid #a8baf0;
+  }
+  .signUpform .form-group{
+    display: flex;
+    flex-direction: column;
+  }
+  .signUpform button{
+    width: 123px;
+    height: 43px;
+    margin-top: 10px;
+    margin-right: 36px;
+    border-radius: 21px;
+    border: none;
+    background-color: #a8baf0;
+    color: white;
+    font-size: 18px;
+  }
+  .signUpform label{
+    margin-left: 12px;
+    font-size: 18px;
+    font-weight: 100;
+    margin-top: 9px;
+    color: #6a6a6a;    
+  }
+  
+</style>
